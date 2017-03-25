d6c76a0b77e5044e00675e03112832859eb56c75
Erick Eduardo Petrucelli <erickpetru@gmail.com>
2017-03-25 16:31:15 -0300

    Changed start point to use process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT ? process.env.PORT : app.get('port');
+
 const server = app.listen(port);
 
 server.on('listening', () =>
